6289d8e321c185af07ec5dd010b8c375b067c333
Martí Serratosa Sedó <martiserratosasedo@Mactea.local>
2020-12-28 23:35:53 +0100

    Prova test

--- test/data/HealthCardIDTest.java
+++ test/data/HealthCardIDTest.java
@@ -1,7 +1,20 @@
 package data;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HealthCardIDTest {
+    @Test
+    void constructor_and_getters() {
+        HealthCardID healthCardID = new HealthCardID("01");
+        assertEquals("01", healthCardID.getPersonalID());
+        assertNotEquals("02", healthCardID.getPersonalID());
+    }
 
+    @Test
+    void exceptions() {
+
+    }
+    }
 }
